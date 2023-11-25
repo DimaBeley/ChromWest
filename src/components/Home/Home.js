dd8c2e5f9475af143b61e3709ace1c6bb74d8ef1
DimaBeley <2beyva@gmail.com>
2023-11-25 10:26:29 -0500

    chromwest images, small fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Container, Box, ImageListItem, ImageList, Typography, Modal } from '@mui/material'
+import { chromeImage, nikelImages } from './galleryImages'
+
 // TODO uninstall swiper
 export const Home = () => {
   const modalStyle = {
@@ -9,62 +11,15 @@ export const Home = () => {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 500,
-    maxHeight: 800,
+    maxHeight: 750,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
-    p: 4
+    padding: '4px 4px 0 4px',
+    overflow: 'hidden',
+    outline: 'none'
   }
-  const itemData = [
-    {
-      img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-      title: 'Bed'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-      title: 'Books'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-      title: 'Sink'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-      title: 'Kitchen'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-      title: 'Blinds'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-      title: 'Chairs'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-      title: 'Laptop'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-      title: 'Coffee'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-      title: 'Storage'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-      title: 'Candle'
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-      title: 'Coffee table'
-    }
-  ]
+
+  const allImages = [...chromeImage, ...nikelImages]
   const [open, setOpen] = useState(null)
   const handleOpen = (id) => setOpen(id)
   const handleClose = () => setOpen(null)
@@ -80,13 +35,13 @@ export const Home = () => {
         >{t('gallery')}</Typography>
       </Box>
       <Box sx={{ width: '100%', alignItems: 'center' }}>
-        <ImageList variant="masonry" cols={3} gap={8}>
-          {itemData.map((item) => (
+        <ImageList variant="masonry" cols={4} gap={8}>
+          {allImages.map((item) => (
               <>
-                <ImageListItem key={item.img} onClick={() => handleOpen(item.img)}>
+                <ImageListItem key={item.img} onClick={() => handleOpen(item.img)} sx={{ overflow: 'hidden' }}>
                   <img
-                      srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      src={`${item.img}?w=248&fit=crop&auto=format`}
+                      srcSet={`${item.img}`}
+                      src={`${item.img}`}
                       alt={item.title}
                       loading="lazy"
                   />
@@ -94,12 +49,14 @@ export const Home = () => {
                 <Modal
                     open={open === item.img}
                     onClose={handleClose}
+
                 >
                   <Box sx={modalStyle}>
-                    <img src={`${item.img}?w=248&fit=crop&auto=format`}
+                    <img src={item.img}
                          alt={item.title}
                          loading="lazy"
-                         style={{ width: '100%' }}
+                         style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+                         onClick={handleClose}
                     />
                   </Box>
                 </Modal>
---
+++ src/components/Home/galleryImages.js
@@ -0,0 +1,65 @@
+import img1 from '../../img/gallery/1.jpg'
+import img2 from '../../img/gallery/2.jpg'
+import img3 from '../../img/gallery/3.jpg'
+import img4 from '../../img/gallery/4.jpg'
+import img5 from '../../img/gallery/5.jpg'
+import img6 from '../../img/gallery/6.jpg'
+import img7 from '../../img/gallery/7.jpg'
+import img8 from '../../img/gallery/8.jpg'
+import img9 from '../../img/gallery/9.jpg'
+import img10 from '../../img/gallery/10.jpg'
+import img11 from '../../img/gallery/11.jpg'
+import img12 from '../../img/gallery/12.jpg'
+
+export const chromeImage = [
+  {
+    img: img1,
+    title: 'Chrome'
+  },
+  {
+    img: img2,
+    title: 'Chrome'
+  },
+  {
+    img: img3,
+    title: 'Chrome'
+  },
+  {
+    img: img4,
+    title: 'Chrome'
+  },
+  {
+    img: img5,
+    title: 'Chrome'
+  },
+  {
+    img: img6,
+    title: 'Chrome'
+  }
+]
+export const nikelImages = [
+  {
+    img: img7,
+    title: 'Nikel'
+  },
+  {
+    img: img8,
+    title: 'Nikel'
+  },
+  {
+    img: img9,
+    title: 'Nikel'
+  },
+  {
+    img: img10,
+    title: 'Nikel'
+  },
+  {
+    img: img11,
+    title: 'Nikel'
+  },
+  {
+    img: img12,
+    title: 'Nikel'
+  }
+]
